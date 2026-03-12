70c127e2a68d45c1549928a994f5217978d26552
Fatih Zahrani <fatihzahrani2005@gmail.com>
2026-03-12 13:32:13 +0700

    Update boxbuild.py

--- forms/boxbuild.py
+++ forms/boxbuild.py
@@ -1,1 +1,37 @@
+import streamlit as st
+import pandas as pd
+from datetime import date
+from utils.database import load_database, get_engineers_by_department
 
+def render_boxbuild():
+
+    df = load_database()
+
+    st.title("Product Build Briefing Checklist - BoxBuild")
+
+    pci = st.text_input("PCI FG P/N")
+
+    initial = pci[:2].upper() if pci else ""
+
+    departments = [
+        "Product Engineer",
+        "Process Engineer (SMT)"
+    ]
+
+    st.markdown("## Meeting Attendance")
+
+    col1,col2,col3,col4 = st.columns(4)
+
+    with col1:
+        mtg1 = st.date_input("Mtg Date 1")
+
+    with col2:
+        mtg2 = st.date_input("Mtg Date 2")
+
+    with col3:
+        mtg3 = st.date_input("Mtg Date 3")
+
+    with col4:
+        mtg4 = st.date_input("Mtg Date 4")
+
+    st.markdown("---")
