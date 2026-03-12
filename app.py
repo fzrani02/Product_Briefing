396c4ae6266e3136c24bb7e30f881e6e343f3565
Fatih Zahrani <fatihzahrani2005@gmail.com>
2026-03-12 13:36:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -115,6 +115,53 @@ for dept in departments:
         key=f"{dept}_date"
     )
 
+for dept in departments:
+
+    engineers = get_engineers_by_department(df, initial, dept)
+
+    engineer_list = [""] + engineers["ER"].tolist()
+
+    col1,col2,col3,col4,col5,col6,col7,col8 = st.columns(8)
+
+    with col1:
+        st.write(dept)
+
+    with col2:
+        selected = st.selectbox(
+            "ER",
+            engineer_list,
+            key=f"{dept}_er"
+        )
+
+    email = ""
+
+    if selected:
+        email = engineers[engineers["ER"] == selected]["Email"].iloc[0]
+
+    with col3:
+        st.text_input(
+            "Email",
+            value=email,
+            disabled=True,
+            key=f"{dept}_email"
+        )
+
+    with col4:
+        st.text_input("Ext", key=f"{dept}_ext")
+
+    with col5:
+        st.checkbox("", key=f"{dept}_m1")
+
+    with col6:
+        st.checkbox("", key=f"{dept}_m2")
+
+    with col7:
+        st.checkbox("", key=f"{dept}_m3")
+
+    with col8:
+        st.checkbox("", key=f"{dept}_m4")
+
+
 
 
 
