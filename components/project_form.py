f96fc05e537018c8f9cf94e9baa2abc3ff470e7f
Fatih Zahrani <fatihzahrani2005@gmail.com>
2026-03-12 16:26:50 +0700

    Update project_form.py

--- components/project_form.py
+++ components/project_form.py
@@ -1,2 +1,34 @@
-import pandas as pd
 import streamlit as st
+from datetime import date 
+
+def render_project_form():
+  col1, col2, col3 = st.columns(3)
+  
+  with col1:
+    project_name = st.text_input("Project Name")
+    build_type = st.text_input("Build Type")
+    date_updated = st.date_input("Date Updated", value=date.today())
+
+  with col2:
+    pci = st.text_input("PCI FG P/N")
+    customer = st.text_input("Customer", disabled=True)
+    project_account = st.text_input("Project Account")
+
+  with col3:
+    revision = st.text_input("Revision", disabled=True)
+    product_type = st.selectbox(
+      "Product Type",
+      ["Automotive","Medical","Military","Consumer"]
+    )
+  
+  return {
+    "project_name": project_name,
+    "build_type": build_type,
+    "data_updated": date_updated, 
+    "pci": pci,
+    "customer": customer,
+    "project_account": project_account,
+    "revision": revision,
+    "product_type": product_type
+  }
+  
