c870f985d96dcc25c21a53c0e621ff7ca1083944
Fatih Zahrani <fatihzahrani2005@gmail.com>
2026-03-12 16:14:16 +0700

    Update header.py

--- components/header.py
+++ components/header.py
@@ -1,2 +1,31 @@
-import pandas as pd
 import streamlit as st
+
+def render_header():
+
+    col1, col2, col3 = st.columns([1,4,1])
+
+    with col1:
+        st.image("logo.png", width=120)
+
+    with col2:
+        st.markdown(
+            """
+            <h1 style='text-align:center'>
+            PRODUCT BUILD BRIEFING CHECKLIST
+            </h1>
+            """,
+            unsafe_allow_html=True
+        )
+
+    with col3:
+        st.markdown(
+            """
+            <div style='text-align:right'>
+            NP-F-006<br>
+            20 Feb 2026
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+
+    st.markdown("---")
