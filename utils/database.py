ec6ad35368233b1c71dfe73c12b72ef2ff826db3
Fatih Zahrani <fatihzahrani2005@gmail.com>
2026-03-12 11:08:17 +0700

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,2 +1,23 @@
 import pandas as pd
-import streamlit as st
+
+def load_database():
+    df = pd.read_csv("engineer_database.csv")
+    return df
+
+
+def get_customer_by_initial(df, initial):
+    result = df[df["Initial"] == initial]
+
+    if not result.empty:
+        return result.iloc[0]["Customer"]
+
+    return ""
+
+
+def get_engineers(df, initial, department):
+    filtered = df[
+        (df["Initial"] == initial) &
+        (df["Department"] == department)
+    ]
+
+    return filtered[["ER", "Email"]]
