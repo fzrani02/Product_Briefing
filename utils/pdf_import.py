9080d2123825195ed0e35893059699755bacecd2
Fatih Zahrani <fatihzahrani2005@gmail.com>
2026-03-13 13:20:06 +0700

    Update pdf_import.py

--- utils/pdf_import.py
+++ utils/pdf_import.py
@@ -1,2 +1,19 @@
-import pandas as pd
-import streamlit as st
+import pdfplumber
+
+def read_pdf(uploaded_file):
+
+    text = ""
+
+    with pdfplumber.open(uploaded_file) as pdf:
+        for page in pdf.pages:
+            text += page.extract_text() + "\n"
+
+    return text
+
+
+def parse_form(text):
+
+    return {
+        "revision": None,
+        "attendance": {}
+    }
