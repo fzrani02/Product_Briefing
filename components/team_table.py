0fcc92516f54f55217fa0dc863825a9fe95bd4f3
Fatih Zahrani <fatihzahrani2005@gmail.com>
2026-03-13 15:49:35 +0700

    Update team_table.py

--- components/team_table.py
+++ components/team_table.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from utils.database import get_engineers_by_department
 
-def render_team_table(df, initial, departments, editable_col, attendance_data, title):
+def render_team_table(df, initial, departments, editable_col, attendance_data, title, key_prefix):
 
     # CSS
     st.markdown("""
@@ -35,7 +35,7 @@ def render_team_table(df, initial, departments, editable_col, attendance_data, t
     left, right = st.columns([3,2])
 
     with left:
-        st.markdown(f"###{title}")
+        st.markdown(f"### {title}")
 
         col1,col2,col3,col4 = st.columns([3,3,2,3])
         col1.markdown("**Department**")
@@ -51,16 +51,16 @@ def render_team_table(df, initial, departments, editable_col, attendance_data, t
         col1,col2,col3,col4 = st.columns(4)
 
         with col1:
-            st.date_input("", key="mtg1", disabled=editable_col != 1, label_visibility="collapsed")
+            st.date_input("", key=f"{key_prefix}_mtg1", disabled=editable_col != 1, label_visibility="collapsed")
 
         with col2:
-            st.date_input("", key="mtg2", disabled=editable_col != 2, label_visibility="collapsed")
+            st.date_input("", key=f"{key_prefix}_mtg2", disabled=editable_col != 2, label_visibility="collapsed")
 
         with col3:
-            st.date_input("", key="mtg3", disabled=editable_col != 3, label_visibility="collapsed")
+            st.date_input("", key=f"{key_prefix}_mtg3", disabled=editable_col != 3, label_visibility="collapsed")
 
         with col4:
-            st.date_input("", key="mtg4", disabled=editable_col != 4, label_visibility="collapsed")
+            st.date_input("", key=f"{key_prefix}_mtg4", disabled=editable_col != 4, label_visibility="collapsed")
 
     st.markdown("---")
 
@@ -132,6 +132,7 @@ def render_team_table(df, initial, departments, editable_col, attendance_data, t
             with col4:
                 checked = attendance_data.get(dept, {}).get("m4", False)
                 st.checkbox("", value=checked, key=f"{dept}_m4", disabled=editable_col != 4)
+
 
 
 
