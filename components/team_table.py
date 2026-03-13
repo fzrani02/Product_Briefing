cdd0db995afaebda028df8c6da8a6ce87343a5f3
Fatih Zahrani <fatihzahrani2005@gmail.com>
2026-03-13 10:30:16 +0700

    Update team_table.py

--- components/team_table.py
+++ components/team_table.py
@@ -1,75 +1,88 @@
 import streamlit as st
 from utils.database import get_engineers_by_department
 
-
 def render_team_table(df, initial, departments, editable_col):
 
-    # header tabel
-    st.markdown("""
-    <table style="width:100%; border-collapse: collapse; text-align:center;">
-    <tr>
-        <th rowspan="2">Department</th>
-        <th rowspan="2">Name</th>
-        <th rowspan="2">Ext. #</th>
-        <th rowspan="2">Email</th>
-        <th colspan="4">Attendances</th>
-    </tr>
-    <tr>
-        <th>Mtg</th>
-        <th>Mtg</th>
-        <th>Mtg</th>
-        <th>Mtg</th>
-    </tr>
-    </table>
-    """, unsafe_allow_html=True)
+    st.markdown("## Project Team Members")
 
-    attendance = []
+    # HEADER LAYOUT
+    left, right = st.columns([3,2])
 
+    with left:
+        col1,col2,col3,col4 = st.columns([3,3,2,3])
+        col1.markdown("**Department**")
+        col2.markdown("**Name**")
+        col3.markdown("**Ext. #**")
+        col4.markdown("**Email**")
+
+    with right:
+        st.markdown("### Meeting Attendance")
+
+        col1,col2,col3,col4 = st.columns(4)
+
+        with col1:
+            st.date_input("Mtg Date 1")
+
+        with col2:
+            st.date_input("Mtg Date 2")
+
+        with col3:
+            st.date_input("Mtg Date 3")
+
+        with col4:
+            st.date_input("Mtg Date 4")
+
+    st.markdown("---")
+
+    # ROW DATA
     for dept in departments:
 
         engineers = get_engineers_by_department(df, initial, dept)
-
         engineer_list = [""] + engineers["ER"].tolist()
 
-        col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
+        left, right = st.columns([3,2])
 
-        with col1:
-            st.write(dept)
+        with left:
 
-        with col2:
-            selected = st.selectbox(
-                "Engineer",
-                engineer_list,
-                key=f"{dept}_er"
-            )
-            
-        with col3:
-            st.text_input("Ext", key=f"{dept}_ext")
+            col1,col2,col3,col4 = st.columns([3,3,2,3])
 
-        email = ""
+            with col1:
+                st.write(dept)
 
-        if selected:
-            email = engineers[engineers["ER"] == selected]["Email"].iloc[0]
+            with col2:
+                selected = st.selectbox(
+                    "Engineer",
+                    engineer_list,
+                    key=f"{dept}_engineer"
+                )
 
-        with col4:
-            st.text_input(
-                "Email",
-                value=email,
-                disabled=True,
-                key=f"{dept}_email"
-            )
+            email = ""
+            if selected:
+                email = engineers[engineers["ER"] == selected]["Email"].iloc[0]
 
-        with col5:
-            st.checkbox("", key=f"{dept}_m1")
+            with col3:
+                st.text_input("Ext", key=f"{dept}_ext")
 
-        with col6:
-            st.checkbox("", key=f"{dept}_m2")
+            with col4:
+                st.text_input(
+                    "Email",
+                    value=email,
+                    disabled=True,
+                    key=f"{dept}_email"
+                )
 
-        with col7:
-            st.checkbox("", key=f"{dept}_m3")
+        with right:
 
-        with col8:
-            st.checkbox("", key=f"{dept}_m4")
+            col1,col2,col3,col4 = st.columns(4)
 
+            with col1:
+                st.checkbox("", key=f"{dept}_m1")
 
+            with col2:
+                st.checkbox("", key=f"{dept}_m2")
 
+            with col3:
+                st.checkbox("", key=f"{dept}_m3")
+
+            with col4:
+                st.checkbox("", key=f"{dept}_m4")
