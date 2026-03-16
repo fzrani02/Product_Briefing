6c06847490bef2454f6aed34213d00ac2a5d104b
Fatih Zahrani <fatihzahrani2005@gmail.com>
2026-03-16 09:45:01 +0700

    Update autosave.py

--- utils/autosave.py
+++ utils/autosave.py
@@ -1,7 +1,19 @@
 import json
-import streamlit as st 
+import streamlit as st
+
 
 def autosave():
-  data= dict(st.session_state)
-  with open("autosave.json", "w") as f:
-    json.dump(data, f)
+
+    clean_data = {}
+
+    for key, value in st.session_state.items():
+
+        try:
+            json.dumps(value)  # cek apakah bisa diserialisasi
+            clean_data[key] = value
+
+        except TypeError:
+            clean_data[key] = str(value)  # fallback jadi string
+
+    with open("autosave.json", "w") as f:
+        json.dump(clean_data, f, indent=2)
