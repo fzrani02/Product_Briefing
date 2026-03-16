ea85e64119deb25024198ef66cba2ba5603ae797
Fatih Zahrani <fatihzahrani2005@gmail.com>
2026-03-16 14:08:25 +0700

    Update pdf_export.py

--- utils/pdf_export.py
+++ utils/pdf_export.py
@@ -25,7 +25,15 @@ def generate_pdf(project_data, departments, pcis_departments):
 
     logo = Image("logo.png", width=120, height=40)
 
-
+    header_table = Table([
+        [logo, Paragraph("<b>PRODUCT BRIEFING ATTENDANCE</b>", styles['Title'])]
+    ], colWidths=[140, 400])
+    
+    header_table.setStyle(TableStyle([
+        ("VALIGN",(0,0),(-1,-1),"MIDDLE")
+    ]))
+    
+    elements.append(header_table)
     
     elements.append(Spacer(1,20))
 
@@ -33,8 +41,6 @@ def generate_pdf(project_data, departments, pcis_departments):
     # TITLE
     # =========================
 
-    elements.append(Paragraph("<b>PRODUCT BRIEFING ATTENDANCE</b>", styles['Title']))
-    elements.append(Spacer(1,20))
     
     # =========================
     # PROJECT INFO
